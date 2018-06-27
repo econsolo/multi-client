baca39652b6d3e5a530ead989fe62d119c4d48c4
econsolo <erickconsolo2706@tandersolucoes.com>
2018-06-27 16:36:22 -0300

    Adiciona projeto com o programa

---
+++ MultiLovelyByBoss/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace MultiLovelyByBoss
+{
+    class Program
+    {
+        [DllImport("user32.dll")]
+        static extern IntPtr FindWindow(string windowClass, string windowName);
+
+        [DllImport("user32.dll")]
+        static extern bool SetWindowText(IntPtr hWnd, string text);
+
+        static void Main(string[] args)
+        {
+            List<Process> processos = new List<Process>(Process.GetProcessesByName("elementclient"));
+            int count = 0;
+            foreach (var processo in processos)
+            {
+                SetWindowText(processo.MainWindowHandle, "Multi-Lovely By Boss " + (++count));
+            }
+            Process.Start(Directory.GetCurrentDirectory() + "\\launcher.exe");
+        }
+    }
+}
